30b4222609dbb825493664d33e25d765b6f7ed93
Maximiliano Carabes <carabesmaximiliano@gmail.com>
2024-05-04 17:44:47 -0300

    Segunda pre-entrega Js

--- js/main.js
+++ js/main.js
@@ -1,33 +1,26 @@
 
+let bebidas = [
+        { id: 1, nombre: "Fanta" },
+        { id: 2, nombre: "Coca-Cola" },
+        { id: 3, nombre: "Sprite" },
+        { id: 4, nombre: "Pepsi" },
+        { id: 5, nombre: "Manaos" },
+        { id: 6, nombre: "Pritty" }
+];
+
 let marcas;
 
 function elegiTuBebida() {
-        let bebidas;
+        let seleccionValida = false;
         do {
-                bebidas = parseInt(prompt("Hola! Por favor selecciona tu bebida favorita:\n1. Fanta\n2. Coca-Cola\n3. Sprite\n4. Pepsi\n5. Manaos\n6. Pritty"));
-                switch (bebidas) {
-                        case 1:
-                                marcas = "Fanta";
-                                break;
-                        case 2:
-                                marcas = "Coca-Cola";
-                                break;
-                        case 3:
-                                marcas = "Sprite";
-                                break;
-                        case 4:
-                                marcas = "Pepsi";
-                                break;
-                        case 5:
-                                marcas = "Manaos";
-                                break;
-                        case 6:
-                                marcas = "Pritty";
-                                break;
-                        default:
-                                alert("Por favor, selecciona una opción válida.");
+                let seleccion = parseInt(prompt("Hola! Por favor selecciona tu bebida favorita:\n1. Fanta\n2. Coca-Cola\n3. Sprite\n4. Pepsi\n5. Manaos\n6. Pritty"));
+                if (seleccion >= 1 && seleccion <= 6) {
+                        marcas = bebidas.find(bebida => bebida.id === seleccion).nombre;
+                        seleccionValida = true;
+                } else {
+                        alert("Por favor, selecciona una opción válida.");
                 }
-        } while (isNaN(bebidas) || bebidas < 1 || bebidas > 6);
+        } while (!seleccionValida);
         console.log("Has seleccionado:", marcas);
 }
 
@@ -35,13 +28,12 @@ function preguntarOtraVez() {
         return confirm("Has seleccionado " + marcas + ". ¿Deseas elegir otra bebida?");
 }
 
-
 elegiTuBebida();
-
 
 while (preguntarOtraVez()) {
         elegiTuBebida();
 }
 
 alert("Gracias por tu selección.");
+
 
